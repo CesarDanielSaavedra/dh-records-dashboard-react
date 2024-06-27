f08147a4a3b6a2d410da9e6df25606bc436bd76d
Saavedra Cesar Daniel <cesardsaavedra@gmail.com>
2024-06-27 17:26:13 -0300

    Se comienza a utilizar useState y fetch para traer la informacion de la API

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,51 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const API_URL = 'http://localhost:3030/apis/products';
+
 import Header from './Components/Header'
 import ProductList from './Components/ProductList'
-import laEsquinaDelInfinito from './assets/laEsquinaDelInfinito.jpg'
-import detonadorDeSueños from './assets/detonadorDeSueños.jpg'
 
 import TotalCardStat from './Components/TotalCardStat'
 import { faCompactDisc, faUsers, faMusic } from '@fortawesome/free-solid-svg-icons'
 
-import productsListAll from './apiData/productsListAll.json'
 
-function App() {
+function App() { 
+
   const [count, setCount] = useState(0)
 
-  console.log(productsListAll);
+  const [productos, setProductos] = useState([]);
 
-  let productos = productsListAll.products;
+  const fetchData = async () => {
+    try {
+      const response = await fetch(API_URL); // Realiza la solicitud HTTP GET a la API
+      if (!response.ok) { // Verifica si el estado HTTP indica éxito
+        throw new Error(`HTTP error! status: ${response.status}`); // Lanza un error con el estado HTTP
+      }
+      const data = await response.json(); // Convierte la respuesta a formato JSON
+      setCount((count) => count + 1);
+      setProductos(data); // Establece los datos de los productos en el estado
+    } catch (error) {
+      console.error('Error:', error); // Maneja el error de red o HTTP
+      // Aquí podrías manejar errores, como mostrar un mensaje al usuario
+    }
+  };
 
-  let totalProductos = productsListAll.total;
+  console.log(productos);
+
+  let totalProductos = productos.total;
   let totalUsuarios = 15;
   let totalGeneros = 7;
 
   return (
     <>
-      
       <Header/>
+
+      <div className="card">
+        <button onClick={fetchData}>
+          Cargar métricas {count}
+        </button>
+      </div>
 
       <TotalCardStat title="Productos" icon={faCompactDisc} total={totalProductos} />
       <TotalCardStat title="Usuarios" icon={faUsers} total={totalUsuarios} />
@@ -45,16 +65,6 @@ function App() {
       </div>
       <h1>Vite + React</h1>
 
-      
-
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
