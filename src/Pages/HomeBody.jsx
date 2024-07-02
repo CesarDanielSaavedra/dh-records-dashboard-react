307597809a0685f12e5ff7081492c42ea4930164
MarianoXinos1 <xinosmariano@gmail.com>
2024-07-01 23:48:03 -0300

    Add: Se creo /users para hacer una lista de ellos a traves de una Api

--- src/Pages/HomeBody.jsx
+++ src/Pages/HomeBody.jsx
@@ -5,8 +5,10 @@ import TotalCardStat from '../Components/TotalCardStat';
 import { faCompactDisc, faUsers, faMusic } from '@fortawesome/free-solid-svg-icons';
 
 const PRODUCTS_API_URL = 'http://localhost:3030/apis/products';
+const USERS_API_URL = 'http://localhost:3030/apis/users';
 
 import ProductList from './ProductList';
+
 
 const HomeBody = () => {
 
@@ -15,7 +17,7 @@ const HomeBody = () => {
             <h3 className='home__contenedor-titulo'>Bienvenidos a las metricas generales de DH-records!</h3>
 
             <TotalCardStat title="Products" icon={faCompactDisc} api={PRODUCTS_API_URL} />
-        {/* <TotalCardStat title="Usuarios" icon={faUsers} total={totalUsuarios} /> */}
+             <TotalCardStat title="Users" icon={faUsers} total={USERS_API_URL} /> 
             <TotalCardStat title="Genres" icon={faMusic} api={PRODUCTS_API_URL} />
       
             {/* <ProductList />  */}
---
+++ src/Pages/UsersList.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import CardUsers from '../Components/CardUsers';
+import './UsersList.css';
+import { Link } from 'react-router-dom';
+
+const API_URL = 'http://localhost:3030/apis/users';
+
+const UsersList = () => {
+
+    const [users, setUsers] = useState(null);
+
+    useEffect (() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(API_URL);
+                if(!response.ok) {                                              // ok = Indica si la solicitud fue exitosa o no
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                const data = await response.json();
+                setUsers(data.users);
+
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        };
+
+        fetchData();
+
+    }, []);
+
+        if(!users) {
+            return <p>No hay usuarios disponibles.</p>;
+        }
+
+        return(
+            <section className='Lista-usuario__contenedor-general'>
+                {users.map((users) => (
+                        <Link to={`/users/${users.id}`} key={users.id}>   
+                        <CardUsers data={users}/>
+                    </Link>                                                      //link = crea enlaces navegables sin recargar la página.
+                ))}  
+                </section>
+
+        )
+
+}
+
+export default UsersList;
